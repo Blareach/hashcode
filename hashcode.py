712d6db3ac663c3cdc22a8c865bc1cc90510c430
Benjamin Morali <morali_b@epita.fr>
2016-02-11 19:39:51 +0100

    merge

--- hashcode.py
+++ hashcode.py
@@ -12,6 +12,7 @@ def parse1(path):
             max_pl = tmp[4]
     return rows, columns, nb_drones, nb_turns, max_pl
 
+<<<<<<< 55c507c1461cb10aafbe3d64b2d8a4c4fcf6ddfb
 def parse3(path):
   f = open(path)
   pro = list()
@@ -22,3 +23,22 @@ def parse3(path):
 
 
 print(parse3("busy_day.in"))
+=======
+def parse2(path):
+    f = open(path)
+    for i, line in enumerate(f):
+        if i == 1:
+            tmp = line.split()
+            nb_diff_prod = tmp[0]
+    return nb_diff_prod
+
+def parse4(path):
+    f = open(path)
+    for i, line in enumerate(f):
+        if i == 3:
+            tmp = line.split()
+            nb_house = tmp[0]
+    return nb_house
+
+print(parse1("busy_day.in"))
+>>>>>>> LOL
